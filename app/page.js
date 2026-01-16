1d0605ef99d09715554a66c1a5fe249c00768310
Ashlyn Knox <ashx3s@tuta.io>
2026-01-16 08:24:16 -0700

    added js variable demo

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 export default function Home() {
   // react uses JSX which is like HTML + Javascript
+  const helloMessage = "Good Morning CPRG306 Section C";
   // anything visual goes in the return
   return (
     <main>
-      <h1 className="text-3xl lg:text-6xl font-bold">Hello World</h1>
+      <h1 className="text-3xl lg:text-6xl font-bold">
+        {helloMessage.toUpperCase()}
+      </h1>
     </main>
   );
 }
