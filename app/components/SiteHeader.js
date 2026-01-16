66a3e9f9e84cab3b02a86acbc8635305041b2115
Ashlyn Knox <ashx3s@tuta.io>
2026-01-16 11:02:36 -0700

    Link example in react

--- app/components/SiteHeader.js
+++ app/components/SiteHeader.js
@@ -1,10 +1,15 @@
+import Link from "next/link";
 export default function SiteHeader() {
   return (
     <header className="flex gap-4 justify-between py-4 px-8">
-      <div>Site Logo</div>
+      <div>
+        <Link href="/">Site Logo</Link>
+      </div>
       <nav>
         <ul className="flex gap-4">
-          <li>Link 1</li>
+          <li>
+            <Link href="week-2">Week 2</Link>
+          </li>
           <li>Link 2</li>
           <li>Link 3</li>
         </ul>
---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <header>
+        <h1>Week 2</h1>
+      </header>
+    </main>
+  );
+}
